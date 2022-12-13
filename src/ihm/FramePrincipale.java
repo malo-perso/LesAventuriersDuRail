4b3b66970a6f8e51a9f940e491eb2d2153e8f5dd
Alexandre-Crespin <84849257+Alexandre-Crespin@users.noreply.github.com>
2022-12-13 15:55:32 +0100

    Modif FramePrincipale

--- src/ihm/FramePrincipale.java
+++ src/ihm/FramePrincipale.java
@@ -1,60 +1,97 @@
-package livrable.IHM;
-import livrable.Controleur;
-import livrable.IHM.asuppr.PanelCreationMappe;
+//package ihm;
 
 import javax.swing.*;
+
 import java.awt.*;
-import java.awt.Dimension;
+import java.awt.event.*;
+import java.io.*;
 
-public class FramePrincipale extends JFrame 
+public class FramePrincipale extends JFrame implements ActionListener
 {
-	private PanelAccueil panelAccueil;
-	private Image        imgBackground;
-	private Controleur ctrl;
-	private PanelConcepteur pnlConcepteur;
+	private JMenuBar menuBarre;
+
+    private JMenu menuFichier;
+
+    private JMenu menuAide;
+
+    private JMenuItem menuNouveau;
+
+    private JMenuItem menuOuvrir;
+
+    private JMenuItem menuRegles;
+
+    private File reglePDF;
+
+    private Image imgLogo;
 
 
-	public FramePrincipale(Controleur ctrl)
-	{
-		this.ctrl = ctrl;
-		//parametres de la fenetre
-		Dimension tailleEcran = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        int hauteur = (int) (tailleEcran.getHeight());
-        int largeur = (int) (tailleEcran.getWidth());
+	public FramePrincipale(){
 
-		this.setSize(largeur, hauteur);
-		this.setTitle("Les aventuriers du IIIe Reich");
-		this.setLocation(500, 500);
+        Toolkit kit = Toolkit.getDefaultToolkit();
 
-		// Création des panels 
-		this.panelAccueil = new PanelAccueil(this);
-		this.pnlConcepteur = new PanelConcepteur(this.ctrl);
+        this.setTitle("Concepteur de Plateau");
+        this.setResizable(false);
+        this.setUndecorated(false);
 
-		// Positionnement des composants
-		this.setContentPane(this.panelAccueil);
+        this.imgLogo = kit.getImage("../data/images/logo.png") ;
+		this.setIconImage(imgLogo);
 
-		// Affichage de la fenêtre
-		this.setResizable(false);
+        this.menuBarre = new JMenuBar();
+
+        this.menuFichier = new JMenu ("Fichier");
+        this.menuNouveau = new JMenuItem("Nouveau");
+        this.menuOuvrir = new JMenuItem("Ouvrir");
+
+        this.menuAide = new JMenu("Aide");
+        this.menuRegles = new JMenuItem("Regles");
+
+        this.menuRegles.setIcon( new ImageIcon( "../data/images/Regles.png"   ) );
+        
+        this.menuFichier.add(menuNouveau);
+        this.menuFichier.add(menuOuvrir);
+
+        this.menuAide.add(menuRegles);
+
+        this.menuBarre.add(menuFichier);
+        this.menuBarre.add(menuAide);
+
+        this.setJMenuBar(menuBarre);
+
+        this.reglePDF = new File("../data/PDF/Regles.pdf");
+
+        this.menuRegles.addActionListener(this::actionPerformed);
+        this.menuFichier.addActionListener(this::actionPerformed);
+        this.menuOuvrir.addActionListener(this::actionPerformed);
+
+
+        this.pack();
+		this.setDefaultLookAndFeelDecorated(true);
+		this.setExtendedState(this.MAXIMIZED_BOTH);
+
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
-		this.pack();
-	}
-
-	public void changerConcepteur()
-	{
-		this.setSize(1200,800);
-		this.setLocation(50,50);
-		this.setContentPane(this.pnlConcepteur);
-		this.revalidate();
-	}
 
 
-
-	
-    public void paintComponent(Graphics g)
-    {
-        g.drawImage(this.imgBackground,0,0,null);
     }
+
+    public void actionPerformed( ActionEvent e){
+
+        if(e.getSource() == this.menuRegles){
+
+            try{
+                System.out.println("Aide");
+                Desktop.getDesktop().open(reglePDF);
+            }catch(Exception erreur){erreur.printStackTrace();}
+        }
+        /*if(e.getSource() == this.menuNouveau){
+            try{
+
+            }catch(){}
+        }*/
+    }
+
+    public static void main(String[] arg){new FramePrincipale();}
+
 
 }
 
