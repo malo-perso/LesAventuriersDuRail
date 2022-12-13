5a17233e96b959f08ad13ee762dea40b007240d5
Alexandre-Crespin <84849257+Alexandre-Crespin@users.noreply.github.com>
2022-12-13 14:38:09 +0100

    Ajout FrameCreation ouverture regles

---
+++ src/Concepteur/livrable/IHM/FrameCreation.java
@@ -0,0 +1,91 @@
+//package IHM;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+
+public class FrameCreation extends JFrame implements ActionListener {
+
+    private JMenuBar menuBarre;
+
+    private JMenu menuFichier;
+
+    private JMenu menuAide;
+
+    private JMenuItem menuNouveau;
+
+    private JMenuItem menuOuvrir;
+
+    private JMenuItem menuRegles;
+
+    private File reglePDF;
+
+    private Image imgLogo;
+
+    public FrameCreation(){
+
+        Toolkit kit = Toolkit.getDefaultToolkit();
+
+        this.setTitle("Concepteur de Plateau");
+        this.setResizable(false);
+        this.setUndecorated(false);
+
+        this.imgLogo = kit.getImage("../images/logo.png") ;
+		this.setIconImage(imgLogo);
+
+        this.menuBarre = new JMenuBar();
+
+        this.menuFichier = new JMenu ("Fichier");
+        this.menuNouveau = new JMenuItem("Nouveau");
+        this.menuOuvrir = new JMenuItem("Ouvrir");
+
+        this.menuAide = new JMenu("Aide");
+        this.menuRegles = new JMenuItem("Regles");
+
+        this.menuRegles.setIcon( new ImageIcon( "../images/Regles.png"   ) );
+        
+        this.menuFichier.add(menuNouveau);
+        this.menuFichier.add(menuOuvrir);
+
+        this.menuAide.add(menuRegles);
+
+        this.menuBarre.add(menuFichier);
+        this.menuBarre.add(menuAide);
+
+        this.setJMenuBar(menuBarre);
+
+        this.reglePDF = new File("../PDF/Regles.pdf");
+
+        this.menuRegles.addActionListener(this::actionPerformed);
+        this.menuFichier.addActionListener(this::actionPerformed);
+        this.menuOuvrir.addActionListener(this::actionPerformed);
+
+
+        this.pack();
+		this.setDefaultLookAndFeelDecorated(true);
+		this.setExtendedState(this.MAXIMIZED_BOTH);
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+
+
+    }
+
+    public void actionPerformed( ActionEvent e){
+
+        if(e.getSource() == this.menuRegles){
+
+            try{
+                System.out.println("Aide");
+                Desktop.getDesktop().open(reglePDF);
+            }catch(Exception erreur){erreur.printStackTrace();}
+        }
+    }
+
+    public static void main(String[] arg){new FrameCreation();}
+
+
+}
