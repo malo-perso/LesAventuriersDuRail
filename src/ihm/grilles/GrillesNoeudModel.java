73916c1b11ae526ec8d016add4204e0f436567e4
HirsuitCadet <axel.brazeau@gmail.com>
2022-12-16 10:47:08 +0100

    JTable remarche

--- src/ihm/grilles/GrillesNoeudModel.java
+++ src/ihm/grilles/GrillesNoeudModel.java
@@ -12,82 +12,46 @@ public class GrillesNoeudModel extends AbstractTableModel
     private Controleur ctrl;
 
 	private String[]   tabEntetes;
-	private Object[][] tabDonnees;
+    ArrayList<Noeud> lstNoeuds;
 
     public GrillesNoeudModel(Controleur ctrl)
     {
         this.ctrl = ctrl;
 
         Noeud n;
-		ArrayList<Noeud> lstNoeuds = this.ctrl.lstNoeudIHMtoXML();
-
-		this.tabDonnees = new Object[lstNoeuds.size()][5];
-        
-        if(this.ctrl.lstNoeudXMLtoIHM() != null)
-        {
-            for (int lig=0; lig<lstNoeuds.size(); lig++)
-            {
-                n = lstNoeuds.get(lig);
-                
-                this.tabDonnees[lig][0] = n.getNom();
-                this.tabDonnees[lig][1] = n.getX();
-                this.tabDonnees[lig][2] = n.getY();
-                this.tabDonnees[lig][3] = n.getNomX();
-                this.tabDonnees[lig][4] = n.getNomY();
-            }
-
-        }
+		lstNoeuds = this.ctrl.lstNoeudIHMtoXML();
         
         this.tabEntetes = new String[] { "Nom", "X", "Y", "Nom X", "Nom Y"};
     }
 
     
     public int    getColumnCount()                 { return this.tabEntetes.length;      }
-	public int    getRowCount   ()                 { return this.tabDonnees.length;      }
+	public int    getRowCount   ()                 { return this.lstNoeuds.size();      }
 	public String getColumnName (int col)          { return this.tabEntetes[col];        }
-	public Object getValueAt    (int row, int col) { return this.tabDonnees[row][col];   }
+	
+    public Object getValueAt    (int row, int col) 
+    { 
+        switch(col)
+        {
+            case 0: return this.lstNoeuds.get(row).getNom();
+            case 1: return String.valueOf(this.lstNoeuds.get(row).getX());
+            case 2: return String.valueOf(this.lstNoeuds.get(row).getY());
+            case 3: return this.lstNoeuds.get(row).getNomX();
+            case 4: return this.lstNoeuds.get(row).getNomY();
+            default: return null;
+        }
+    }
+
 	public Class  getColumnClass(int c)            { return getValueAt(0, c).getClass(); }
 
     public void majTable(ArrayList<Noeud> lstNoeuds)
     {   
-        Noeud n;
-
-        tabDonnees = new Object[lstNoeuds.size()][5];
-        
-        if(this.ctrl.lstNoeudXMLtoIHM() != null)
-        {
-            for (int lig=0; lig<lstNoeuds.size(); lig++)
-            {
-                n = lstNoeuds.get(lig);
-                
-                this.tabDonnees[lig][0] = n.getNom();
-                this.tabDonnees[lig][1] = n.getX();
-                this.tabDonnees[lig][2] = n.getY();
-                this.tabDonnees[lig][3] = n.getNomX();
-                this.tabDonnees[lig][4] = n.getNomY();
-            }
-
-        }
-        
-        this.tabEntetes = new String[] { "Nom", "X", "Y", "Nom X", "Nom Y"};
+        this.lstNoeuds = lstNoeuds; 
+        this.fireTableRowsInserted(0, this.lstNoeuds.size() - 1);
     }
 
     public void setValueAt(Object value, int row, int col)
     {
         boolean bRet;
-
-        if ( col > 0 )
-		{
-			bRet = this.ctrl.majPosNoeud ( row, col, (Integer) value );
-			if ( bRet )
-			{
-				this.tabDonnees[row][col] = value;
-				this.fireTableCellUpdated(row, col);
-			}
-		}
-        else {
-            this.tabDonnees[row][col] = value;
-			this.fireTableCellUpdated(row, col);
-        }
     }
 }
