3986571ee8927852774dfe336de338356574a9d3
malo <77111396+malo-perso@users.noreply.github.com>
2023-01-04 16:13:31 +0100

    Frame principale
    création de la FramePrincipale avec tous les panel

---
+++ src/ihm/PanelMainJoueur.java
@@ -0,0 +1,15 @@
+package src.ihm;
+
+import javax.swing.JPanel;
+
+import src.Controleur;
+
+public class PanelMainJoueur extends JPanel {
+    
+    private Controleur ctrl;
+
+    PanelMainJoueur(Controleur ctrl) {
+
+        this.ctrl = ctrl;
+    }
+}
---
+++ src/ihm/PanelPioche.java
@@ -0,0 +1,15 @@
+package src.ihm;
+
+import javax.swing.JPanel;
+
+import src.Controleur;
+
+public class PanelPioche extends JPanel  {
+    
+    private Controleur ctrl;
+
+    PanelPioche(Controleur ctrl) {
+
+        this.ctrl = ctrl;
+    }
+}
