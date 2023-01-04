73cfab03e5656e1eb7523f637a0fa1d8557f4aac
GloubyBoulga <81560542+GloubyBoulga@users.noreply.github.com>
2023-01-04 16:53:46 +0100

    panelPioche et méthodes dans Controleur

---
+++ src/ihm/PanelPioche.java
@@ -0,0 +1,50 @@
+package src.ihm;
+
+import javax.swing.*;
+
+import java.awt.GridLayout;
+
+import java.util.ArrayList;
+
+import src.Controleur;
+import src.metier.CarteVehicule;
+import src.metier.CarteObjectif;
+
+public class PanelPioche extends JPanel {
+    
+    Controleur ctrl;
+    ArrayList<CarteVehicule> piocheVehicule;
+    ArrayList<CarteObjectif> piocheObjectif;
+    CarteVehicule[] piocheVehiculeVisible;
+    
+
+    public PanelPioche(Controleur ctrl) {
+        this.ctrl = ctrl;
+        this.piocheVehicule = ctrl.getLstCartesVehicule();
+        this.piocheObjectif = ctrl.getLstCartesObjectif();
+        this.piocheVehiculeVisible = this.getPiocheVehiculeVisible();
+
+        this.setLayout(new GridLayout(1, 7));
+
+        for (int i = 0; i < this.piocheVehiculeVisible.length; i++) {
+            this.add(new JLabel(this.piocheVehiculeVisible[i].getType().toString()));
+        }
+        this.add(new JButton("Vehicules"));
+        this.add(new JButton("Objectif"));
+    }
+
+    public CarteVehicule[] getPiocheVehiculeVisible() {
+        CarteVehicule[] piocheVehiculeVisible = new CarteVehicule[5];
+        for (int i = 0; i < 5; i++) {
+            piocheVehiculeVisible[i] = this.piocheVehicule.get(i);
+        }
+        return piocheVehiculeVisible;
+    }
+
+    public void majPiocheVehiculeVisible() {
+        this.piocheVehiculeVisible = getPiocheVehiculeVisible();
+    }
+
+
+
+}
